1cd20fb028b4eaba77c305d39e6a5f7c0432469c
Andy Weiss <andyweiss1982@gmail.com>
2015-05-06 16:59:56 -0400

    mount griddler

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'posts/index'
   root 'posts#index'
+  mount_griddler
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
