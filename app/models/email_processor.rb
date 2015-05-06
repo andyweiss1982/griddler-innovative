7af5a64d4cd8b0bf25d68d5d268049e1ed027feb
Andy Weiss <andyweiss1982@gmail.com>
2015-05-06 17:15:40 -0400

    logging puts

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,5 +1,8 @@
 class EmailProcessor
   def self.process(email)
-    Post.create!({ body: email.body, email: email.from })
+    # Post.create!({ body: email.body, email: email.from })
+    puts "============================"
+    puts "email received"
+    puts "============================"
   end
 end
