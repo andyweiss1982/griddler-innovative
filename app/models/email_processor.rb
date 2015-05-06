072b882083aa732658399a2ce82266735a79925a
Andy Weiss <andyweiss1982@gmail.com>
2015-05-06 17:44:39 -0400

    deleted commented out code

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,11 +1,4 @@
 class EmailProcessor
-  # def self.process(email)
-  #   Post.create!({ body: email.body, email: email.from })
-  #   # puts "============================"
-  #   # puts "email received"
-  #   # puts "============================"
-  # end
-
   def initialize(email)
     @email = email
   end
